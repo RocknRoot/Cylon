e30ecab9013fb54707f90d0f49652e06a5bd379b
TibshoOT <tib@rocknroot.org>
2013-06-14 12:17:09 +0200

    Update version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 
 setup(
     name="Cylon",
-    version="0.1",
+    version="0.2",
     description="XMPP bot",
     long_description="XMPP bot fur teh lulz",
     author="Thibaut Deloffre",
